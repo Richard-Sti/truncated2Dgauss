07358db279b7f98f3f1298448284a38b3d11f505
Richard Stiskalek <richard.stiskalek@protonmail.com>
2022-03-12 20:05:59 +0100

    Control random seeds (#1)
    * Update gitignore
    
    * Renaming
    
    * Edit the code
    
    * Add cheks and better docs
    
    * Correct some syntax

--- truncated2Dgauss/dist.py
+++ truncated2Dgauss/dist.py
@@ -13,30 +13,72 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-
 import numpy
 from scipy.stats import multivariate_normal
 
 from .prob_mass import CDFIntegral, is_higher_equal, is_in_bounds
 
 
-class Truncated2DGaussian:
+def list_to_array(x):
+    """
+    Check if `x` is a numpy array, if not convert it to it.
+
+    Arguments
+    ---------
+    x : list or array
+        Input list of array
+
+    Returns
+    -------
+    x : numpy.ndarray
+        Output array.
+    """
+    if isinstance(x, numpy.ndarray):
+        return x
+    else:
+        return numpy.asarray(x)
+
+
+class Truncated2DGauss:
+    """
+    A truncated 2-dimensional Gaussian distribution with probability density
+    function and random sampling methods.
+
+    Arguments
+    ---------
+    lower : 1-dimensional array
+        Lower box limits.
+    upper : 1-dimensional array
+        Upper box limits.
+    random_generator : Generator, optional
+        Random generator. By default ``numpy.random.default_rng()``.
+    atol : float, optional
+        Absolute tolerance for ensuring the covariance matrix is symmetric.
+        By default 1e-8.
+    """
     _lower = None
     _upper = None
     _cdf = None
+    _atol = None
     _cov_hash = numpy.nan
     _dist = None
-    _allow_singular = False
+    _random_generator = None
 
-    def __init__(self, lower, upper, allow_singular=False):
-        if not is_higher_equal(upper, lower):
+    def __init__(self, lower, upper, random_generator=None, atol=1e-8):
+        # Ensure we have numpy arrays
+        self._lower = list_to_array(lower)
+        self._upper = list_to_array(upper)
+        # Which have correct ordering
+        if not is_higher_equal(self._upper, self._lower):
             raise ValueError("`upper={}` must be larger than `lower={}`."
-                             .format(lower, upper))
-        self._lower = lower
-        self._upper = upper
-        self._allw_singular = allow_singular
-        self._cdf = CDFIntegral(lower, upper)
+                             .format(self._lower, self._upper))
+        # Create the random generator
+        if random_generator is None:
+            self._random_generator = numpy.random.default_rng()
+        else:
+            self._random_generator = random_generator
+        self._atol = atol
+        self._cdf = CDFIntegral(self._lower, self._upper)
 
     @property
     def lower(self):
@@ -45,7 +87,7 @@ class Truncated2DGaussian:
 
         Returns
         -------
-        lower : array
+        lower : 1-dimensional array
         """
         return self._lower
 
@@ -56,11 +98,11 @@ class Truncated2DGaussian:
 
         Returns
         -------
-        upper : array
+        upper : 1-dimensional array
         """
         return self._upper
 
-    def _check_new_cov(self, cov):
+    def _check_new_cov(self, cov, allow_singular):
         """
         Check whether the covariance matrix has changed. If yes recalculate
         the internal multivariate normal distribution.
@@ -69,35 +111,81 @@ class Truncated2DGaussian:
         ---------
         cov : 2-dimensional array
             The new covariance matrix.
+        allow_singular : bool, optional
+            Whether to allow a singular covariance matrix.
         """
+        # First ensure that the covariance is symmetric
+        if abs(cov[0, 1] - cov[1, 0]) > self._atol:
+            raise ValueError("Covariance {} is not symmetric.".format(cov))
         new_hash = hash(cov.data.tobytes())
         if new_hash != self._cov_hash:
             self._cov_hash = new_hash
             self._dist = multivariate_normal(
-                cov=cov, allow_singular=self._allow_singular)
+                cov=cov, allow_singular=allow_singular)
+
+    def _is_mean_in_bounds(self, mean):
+        if not is_in_bounds(mean, self.lower, self.upper):
+            return ValueError("`mean={}` is out of bounds.".format(mean))
 
     def logpdf(self, x, mean, cov, allow_singular=False):
         """
         Log probability density at `x`.
-        
-        TODO: finish up the docs
+
+        Arguments
+        ---------
+        x : 1-dimensional array
+            Position to evaluate the PDF.
+        mean : 1-dimensional array
+            Mean of the non-truncated Gaussian.
+        cov : 2-dimensional array
+            Covariance of the non-truncated Gaussian.
+        allow_singular : bool, optional
+            Whether to allow singular covariance. By default ``False``.
+
+        Returns
+        -------
+        out : Float
+            The log probability density at `x`.
         """
-        # Check the mean is in bounds
-        if not is_in_bounds(x, self.lower, self.upper):
-            return ValueError("`mean={}` is out of bounds.".format(mean))
+        # Optionally convert to arrays
+        x = list_to_array(x)
+        mean = list_to_array(mean)
+        cov = list_to_array(cov)
+
+        self._is_mean_in_bounds(mean)
         # Check current position is in bounds
         if not is_in_bounds(x, self.lower, self.upper):
             return -numpy.infty
+
         # Optionally update the distribution
-        self._check_new_cov(cov)
+        self._check_new_cov(cov, allow_singular)
         return self._dist.logpdf(x - mean) - numpy.log(self._cdf(mean, cov))
 
     def pdf(self, x, mean, cov, allow_singular=False):
+        """
+        Probability density at `x`.
+
+        Arguments
+        ---------
+        x : 1-dimensional array
+            Position to evaluate the PDF.
+        mean : 1-dimensional array
+            Mean of the non-truncated Gaussian.
+        cov : 2-dimensional array
+            Covariance of the non-truncated Gaussian.
+        allow_singular : bool, optional
+            Whether to allow singular covariance. By default ``False``.
+
+        Returns
+        -------
+        out : Float
+            The probability density at `x`.
+        """
         return numpy.exp(self.logpdf(x, mean, cov, allow_singular))
 
     def rvs(self, mean, cov):
         """
-        Rejection sampling of a single observation from the trucanted 2D
+        Rejection sampling of a single observation from the truncated 2D
         distribution.
         
         Arguments
@@ -113,10 +201,9 @@ class Truncated2DGaussian:
             The new observation.
         """
         # Check the mean is in bounds
-        if not is_in_bounds(x, self.lower, self.upper):
-            return ValueError("`mean={}` is out of bounds.".format(mean))
-        self._check_new_cov(cov)
+        mean = list_to_array(mean)
+        self._is_mean_in_bounds(mean)
         while True:
-            x = mean + self._dist.rvs()
+            x = self._random_generator.multivariate_normal(mean, cov)
             if is_in_bounds(x, self.lower, self.upper):
                 return x
